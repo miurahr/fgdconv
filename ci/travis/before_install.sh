0113c63e02360df17dd58e68e28403978b24b4c6
Hiroshi Miura <miurahr@linux.com>
2017-04-17 18:03:09 +0900

    Travis: fix locale error when running pip
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- ci/travis/before_install.sh
+++ ci/travis/before_install.sh
@@ -5,15 +5,21 @@ export chroot="$PWD/xenial"
 mkdir -p "$chroot$PWD"
 sudo apt-get -qq update
 sudo apt-get install -y debootstrap
-export LC_ALL=en_US
+export LC_ALL=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_CTYPE=en_US.UTF-8
 sudo debootstrap xenial "$chroot"
 sudo mount --rbind "$PWD" "$chroot$PWD"
 sudo mount --rbind /dev/pts "$chroot/dev/pts"
 sudo mount --rbind /proc "$chroot/proc"
 sudo su -c 'echo "deb http://archive.ubuntu.com/ubuntu xenial universe" >> xenial/etc/apt/sources.list'
+sudo chroot "$chroot" locale-gen en_US.UTF-8
+sudo chroot "$chroot" dpkg-reconfigure locales
 sudo chroot "$chroot" apt-get update
 sudo chroot "$chroot" apt-get install -y software-properties-common python-software-properties
 sudo chroot "$chroot" add-apt-repository -y ppa:ubuntugis/ubuntugis-unstable
 sudo chroot "$chroot" apt-get update
 sudo chroot "$chroot" apt-get install -y libxml2-dev libgdal-dev python2.7 python2.7-dev python-pip python-pip-whl python3 python3-wheel build-essential python3-pip python3.5-dev python-virtualenv python3-virtualenv
 # libgdal-dev 2.1.0
+
--- ci/travis/install.sh
+++ ci/travis/install.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 set -e
 export chroot="$PWD"/xenial
-export LC_ALL=en_US
+export LC_ALL=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_CTYPE=en_US.UTF-8
 
 sudo chroot "$chroot" sh -c "cd $PWD && pip install tox"
 sudo chroot "$chroot" sh -c "cd $PWD && pip install coveralls"
