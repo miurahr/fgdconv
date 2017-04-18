68a7fcad447ae4508b78093ede162f66b0538fc4
Hiroshi Miura <miurahr@linux.com>
2017-04-18 13:06:34 +0900

    Fix travis script to use stable ubuntugis distribution

--- ci/travis/before_install.sh
+++ ci/travis/before_install.sh
@@ -18,7 +18,7 @@ sudo chroot "$chroot" locale-gen en_US.UTF-8
 sudo chroot "$chroot" dpkg-reconfigure locales
 sudo chroot "$chroot" apt-get update
 sudo chroot "$chroot" apt-get install -y software-properties-common python-software-properties
-sudo chroot "$chroot" add-apt-repository -y ppa:ubuntugis/ubuntugis-unstable
+sudo chroot "$chroot" add-apt-repository -y ppa:ubuntugis/ubuntugis-stable
 sudo chroot "$chroot" apt-get update
 sudo chroot "$chroot" apt-get install -y libxml2-dev libgdal-dev python2.7 python2.7-dev python-pip python-pip-whl python3 python3-wheel build-essential python3-pip python3.5-dev python-virtualenv python3-virtualenv
 # libgdal-dev 2.1.0
