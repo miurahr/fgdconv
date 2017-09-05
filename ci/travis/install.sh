acf15c8978da81f365ba0580f3eb2cdeb2ef67af
Hiroshi Miura <miurahr@linux.com>
2017-09-05 21:34:49 +0900

    Test: fix travis test script
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- ci/travis/install.sh
+++ ci/travis/install.sh
@@ -6,5 +6,6 @@ export LANGUAGE=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LC_CTYPE=en_US.UTF-8
 
-sudo chroot "$chroot" sh -c "cd $PWD && pip install tox"
-sudo chroot "$chroot" sh -c "cd $PWD && pip install coveralls"
+sudo -H chroot "$chroot" sh -c "cd $PWD && pip install --upgrade pip"
+sudo -H chroot "$chroot" sh -c "cd $PWD && pip install tox"
+sudo -H chroot "$chroot" sh -c "cd $PWD && pip install coveralls"
